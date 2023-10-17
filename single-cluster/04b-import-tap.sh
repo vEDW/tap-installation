7bf18b940ba89093e2e2338388c46ddca81155d7
Eric De Witte <vEDW@users.noreply.github.com>
2023-10-17 09:19:42 +0200

    import tap
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

---
+++ single-cluster/04b-import-tap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 03-deploy-cluster-essentials.sh
+#
+# This script is only required for non-TKGm clusters (AKS, EKS, GKE, etc.). For vSphere /w Tanzu v7 it's also required to 
+# install Cluster Essentials. vSphere /w Tanzu v8 TKG clusters have cluster essentials available out-of-the-box, so you can skip this step.
+#
+# Run this script in the context of the Kubernetes cluster you're planning to install TAP on.
+# Use 'kubectl config set-context <kubernetes context>' to switch to correct cluster prior to running this script.
+#
+# Download Cluster Essentials from Tanzu Network at https://network.tanzu.vmware.com/
+#
+# For Linux:
+# - Change download file to tanzu-cluster-essentials-linux-amd64-1.3.0.tgz
+# 
+source 00-set-environment-variables.sh  
+
+ClusterEssentialsVersion=$(jq -r '."tap-versions"[] | select (."tap-version" == "'${TAP_VERSION}'") | ."cluster-essentials-bundle"' tanzu_versions.json)
+echo " ClusterEssentialsVersion : ${ClusterEssentialsVersion}"
+
+ClusterEssentialsSHA=$(jq -r '."tap-versions"[] | select (."tap-version" == "'${TAP_VERSION}'") | ."cluster-essentials-sha"' tanzu_versions.json)
+echo "ClusterEssentialsSHA : ${ClusterEssentialsSHA}"
+
+#cd $HOME/tanzu-cluster-essentials
+
+
+if [[ "${TAP_VERSION}" != "" ]];
+then
+    IMGPKG_REGISTRY_HOSTNAME=${MY_REGISTRY} 
+    IMGPKG_REGISTRY_USERNAME=${MY_REGISTRY_USER} 
+    IMGPKG_REGISTRY_PASSWORD=${MY_REGISTRY_PASSWORD} 
+
+    imgpkg copy \
+    --tar $HOME/tanzu-cluster-essentials/tap-${TAP_VERSION}.tar \
+    --to-repo ${MY_REGISTRY}/${MY_REGISTRY_INSTALL_REPO}/${MY_REGISTRY_INSTALL_REPO} \
+    --include-non-distributable-layers \
+    --registry-ca-cert-path ${MY_REGISTRY_CA_PATH}
+else 
+  echo "one or more versions variable is empty"
+fi
