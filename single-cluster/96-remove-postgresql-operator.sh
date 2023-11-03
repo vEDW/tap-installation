e30e0badb6dae669b5eccddf5fe07616793d183e
Eric De Witte <vEDW@users.noreply.github.com>
2023-11-03 16:35:58 +0100

    k
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- single-cluster/96-remove-postgresql-operator.sh
+++ single-cluster/96-remove-postgresql-operator.sh
@@ -3,4 +3,4 @@
 
 helm delete my-postgres-operator
 kubectl delete ns postgresql-operator
-kubectl delete crd $(k get crd | grep postgres | awk '{print $1}')
+kubectl delete crd $(kubectl get crd | grep postgres | awk '{print $1}')
